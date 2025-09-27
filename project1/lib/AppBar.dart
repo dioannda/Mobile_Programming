3e1e2671f466c596a7cfcf62ae8957f422fe1000
dioannda <dioananda275@gmail.com>
2025-09-27 20:19:19 +0700

    add AppBar

---
+++ project1/lib/AppBar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DioAnanda());
+}
+
+class DioAnanda extends StatelessWidget {
+  const DioAnanda({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("AplikasiKu Ini"),
+        ),
+        body: Center(child: Text("Dio Ananda"),),
+      ),
+    );
+  }
+}
