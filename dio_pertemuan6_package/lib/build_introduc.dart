f7c8617a0c4a75de4031423520bf3fab345a6b0c
dioannda <dioananda275@gmail.com>
2025-10-31 23:44:21 +0700

    Add pertemuan 6 package

---
+++ dio_pertemuan6_package/lib/build_introduc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'pages/intro.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Introduction Screen Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const IntroductionPage(),
+    );
+  }
+}
